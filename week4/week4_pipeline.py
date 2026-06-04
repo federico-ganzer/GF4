9587589676b382d448d11f929e4c34e9b58ee03b
Federico Ganzer <fg442@cam.ac.uk>
2026-06-04 10:25:06 +0100

    comments

--- week4/week4_pipeline.py
+++ week4/week4_pipeline.py
@@ -370,13 +370,10 @@ def triangulate_and_append_new_points(
         if reg_id == image_id:
             continue
 
-<<<<<<< HEAD
-=======
         # Retrieve matches between the newly registered image and this registered image
         pair = (reg_id, image_id) if (reg_id, image_id) in all_matches else (image_id, reg_id)
         matches = all_matches.get(pair)
 
->>>>>>> a9e77bf46eba080a30e625f81dc8413e46cfaba5
 def register_next_image(
     week2,
     features,
